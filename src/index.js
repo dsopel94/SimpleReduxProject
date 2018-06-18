3e4b42fb856c5e17da6adc3015b3ae66f07670bc
dsopel94 <dsopel0@gmail.com>
2018-06-17 19:31:42 -0500

    completed input unit section

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import SearchBar from './components/search_bar'
+
 const API_KEY = 'AIzaSyB4U71o66VfrJ6C4JSePiHl8kR87ecl9TU';
 
 const App = () => {
-  return <div>Hi!</div>;
+  return <div>
+    <SearchBar />
+  </div>;
 }
 
 
