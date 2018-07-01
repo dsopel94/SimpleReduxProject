3a80f9e589133d3b0c78ed2a445f17b6ca50e2c8
dsopel94 <dsopel0@gmail.com>
2018-06-30 22:13:14 -0500

    added CSS and throttled search input to make it less choppy

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list';
 import SearchBar from './components/search_bar';
+import VideoDetail from './components/video_detail';
+import _ from 'lodash'
 
 const API_KEY = 'AIzaSyDPZGwMrodCchUDiYvfbf-8j77_dvDM5Qs';
 
@@ -10,19 +12,33 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = { videos: [],
+                   selectedVideo: null
+    };
 
-  YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-    this.setState({ videos })
+  this.videoSearch('surfboards');
+}
+
+videoSearch(term) {
+  YTSearch({key: API_KEY, term: term}, (videos) => {
+    this.setState({ videos: videos,
+    selectedVideo: videos[0]
+    });
   });
 }
 
+
   render() {
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)} , 300);
     return (
       <div>
-        <SearchBar />
-        <VideoList videos={this.state.videos} />
-        </div>
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          videos={this.state.videos}
+          onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+         />
+      </div>
       );
     }
   }
